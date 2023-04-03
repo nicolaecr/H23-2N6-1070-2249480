f41f7e7adcd5a78ada2e4a60b7de943ac932521f
Croitor Nicolae <2249480@cegepmontpetit.ca>
2023-04-03 10:44:55 -0400

    18

--- exercice logiciels/exercice_Ordi_logiciel.py
+++ exercice logiciels/exercice_Ordi_logiciel.py
@@ -1,28 +1,33 @@
 class Ordinateur:
     
     def __init__(self,ID, adresseIP, processeur=None, memoire_vive=None) -> None:
-        pass
+        self.ID = ID
+        self.adresseIP = adresseIP
+        self.processeur = processeur
+        self.memoire_vive =memoire_vive
     
     def __str__(self) -> str:
-        pass
+        return
     
     @classmethod
     def upgrader_processeur(cls, nouveau_processeur) -> None:
-        pass
-    
+        return cls(processeur = nouveau_processeur)
     @classmethod
     def upgrader_memoire(cls, nouvelle_norme) -> None:
-        pass
+        return cls(memoire_vive = nouvelle_norme)
     
    
 class Poste_de_travail(Ordinateur):
     
     def __init__(self,ID, adresseIP, utilisation,processeur=None, memoire_vive=None) -> None:
-        pass
+        super().__init__(self,ID,adresseIP,processeur,memoire_vive)
+        self.utilisation =utilisation
     
     def installer_logiciel(self,logiciel,version) -> None:
-        pass 
-    
+        self.logiciel = logiciel
+        self.version = version
+        
+        pass
     def desinstaller_logiciel(self,logiciel,version) -> None:
         pass 
     
